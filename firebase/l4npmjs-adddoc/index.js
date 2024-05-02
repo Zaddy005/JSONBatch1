057b149f49acebc63905d9368b53e4ad666c5ee7
zaddy <aungnaingphyo005@gmail.com>
2024-05-02 15:00:51 +0630

    firebase

---
+++ firebase/l4npmjs-adddoc/index.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore,collection,addDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCz7A2NAB7Uiew2pxyP-AyFohGZWpsoG_k",
+    authDomain: "my-first-project-dc7dd.firebaseapp.com",
+    projectId: "my-first-project-dc7dd",
+    storageBucket: "my-first-project-dc7dd.appspot.com",
+    messagingSenderId: "654401605320",
+    appId: "1:654401605320:web:ff7c0c2d890e2060495ffd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// console.log(app);
+
+const db = getFirestore(app);
+// console.log(db);
+
+// addDoc(collection(db,"user"),{
+//     name:"aung aung",
+//     city:"Yangon",
+//     age:20
+// });
+
+addDoc(collection(db,'user'),{
+    name:"aung aung",
+    city:"Yangon",
+    age:19
+}).then(docRef=>{
+    console.log(docRef.id);
+}).catch(error=>{
+    console.log(error);
+});
+
+
+// => Create Database
+// > Build > Firebase Database > Create database >
